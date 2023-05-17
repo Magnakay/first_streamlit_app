03150b4d63813cadab475cc6a59334804e75420b
Magnakay <100078366+Magnakay@users.noreply.github.com>
2023-05-17 11:53:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,6 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.stop()
-
 def get_fruit_load_list():
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   with my_cnx.cursor() as my_cur:
